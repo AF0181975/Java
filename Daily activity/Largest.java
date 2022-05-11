e8518b0d14abc51ed50e69c30b21de11324b3d88
Mugesh.B <103984320+AF0181975@users.noreply.github.com>
2022-05-11 09:40:02 +0530

    Add files via upload

---
+++ Daily activity/Largest.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Largest
+{
+public static void main(String args[])
+{
+int a[]=new int[5];
+int max=a[0];
+Scanner s=new Scanner(System.in);
+for(int i=0;i<5;i++)
+{
+System.out.println("Enter the values of array:");
+a[i]=s.nextInt();
+}
+for(int i=1;i<5;i++)
+{
+if(a[i]>max)
+{
+max=a[i];
+}
+}
+System.out.println("Maximum number is:"+max);
+}
+}
+
+
