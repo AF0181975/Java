63f28eedd38f80bb70f0a7a06040e24d2dbfa336
Mugesh.B <103984320+AF0181975@users.noreply.github.com>
2022-05-26 21:48:27 +0530

    Add files via upload

---
+++ lab activity/Floydtriangle.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Floydtriangle {
+
+	public static void main(String[] args)
+	{
+		int n,num=1,a,b;
+		Scanner in = new Scanner(System.in);
+		System.out.println("enter the number of rows of floyd's triangle");
+		n=in.nextInt();
+		System.out.println("Floyd's triangle are:-");
+		for(a=1;a<=n;a++)
+		{
+			for(b=1;b<=a;b++)
+			{
+				
+			
+			System.out.print(num+" ");
+			num++;
+		}
+		System.out.println();
+
+	}
+System.out.println();
+}
+}
